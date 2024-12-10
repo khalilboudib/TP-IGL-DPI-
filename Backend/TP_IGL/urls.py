5b8d25fd643c00acae973e225510518cdfbea793
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-12-10 22:35:59 +0000

    Update URL routing and views for app: replace 'afficher_patient' with 'index' and adjust urlpatterns

--- Backend/TP_IGL/urls.py
+++ Backend/TP_IGL/urls.py
@@ -22,5 +22,5 @@ import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
-    path('__debug__/', include(debug_toolbar.urls))
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
