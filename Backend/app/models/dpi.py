ea16c9082b139f15bf2700ceceeabf4a6b13579e
bndCom <ma_bendaoud@esi.dz>
2024-12-19 09:53:58 +0100

    create /models/*

---
+++ Backend/app/models/dpi.py
@@ -0,0 +1,23 @@
+from django.db import models
+from .users import Patient
+from .khalil_models import Diagnostic, Compte_Rendu, CertificatMedical, Hospitalisation, Soin
+
+class StatutDPI(models.TextChoices):
+    OUVERT = 'OUVERT'
+    EN_COURS = 'EN_COURS'
+    CLOTURE = 'CLOTURE'
+    ARCHIVE = 'ARCHIVE'
+    SUSPENDU = 'SUSPENDU'
+
+
+class DPI(models.Model):
+    id_dpi = models.AutoField(primary_key=True)
+    patient = models.OneToOneField(Patient, on_delete=models.SET_NULL, null=True)
+    date_creation = models.DateTimeField()
+    commentaire_administratif = models.TextField()
+    chemin_QR_code = models.CharField(max_length=255)
+    diagnostic = models.ForeignKey(Diagnostic, on_delete=models.SET_NULL, null=True)
+    compte_Rendu = models.ForeignKey(Compte_Rendu, on_delete=models.SET_NULL, null=True)
+    certificatMedical = models.ForeignKey(CertificatMedical, on_delete=models.SET_NULL, null=True)
+    hospitalisation = models.ForeignKey(Hospitalisation, on_delete=models.SET_NULL, null=True)
+    soin = models.ForeignKey(Soin, on_delete=models.SET_NULL, null=True)
